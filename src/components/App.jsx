1befa8b49d0181662925e780ea5f9c83b8e9f343
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-11-04 11:34:14 +0100

    start work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
+import Home from 'pages/Home';
+import Movies from '../pages/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,9 @@
+const Movies = () => {
+  return (
+    <main>
+      <h1>Our movies</h1>
+    </main>
+  );
+};
+
+export default Movies;
